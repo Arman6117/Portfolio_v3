0cae5e2025c0a1bd10cc8617b58af2de376c59b6
Arman Patel <vanixgaming634@gmail.com>
2023-11-17 19:03:13 +0530

    Project section complete

--- components/ProjectContent.tsx
+++ components/ProjectContent.tsx
@@ -1,17 +1,18 @@
+"use client";
 import Spotify from "../public/images/Spotify.png";
 import Netflix from "../public/images/Netflix.png";
 import Keeper from "../public/images/Keeper.png";
 import Discord from "../public/images/Discord.png";
 
 import ProjectCard from "./ProjectCard";
+import { IoLogoGithub } from "react-icons/io";
 
 const ProjectContent = () => {
+  const handleClick = () => {
+    window.open("https://github.com/Arman6117", "_blank");
+  };
   return (
-    <div className="h-screen w-screen">
-      <div className="flex flex-col ">
-        {/* <div className="w-[100vw] md:-ml-[8vw] sm:-ml-[13vw] xl:-ml-[7vw] hidden sm:block absolute h-[1.5px] bg-neutral-700 rounded-full" />
-        <div className="h-[150vh] absolute hidden sm:block left-[50%] w-[1.5px] bg-neutral-700 rounded-full" /> */}
-      </div>
+    <div className="h-screen w-screen ">
       {/* <div className="w-[100vw] mt-10  -ml-[8vw] sm:top-[131rem] top-[126rem] hidden sm:block absolute h-[1.5px] bg-neutral-700 rounded-full" /> */}
       <div className="grid  sm:ml-2 sm:px-0 gap-10  grid-cols-1 sm:grid-cols-1 mt-10  ">
         <div className="relative ">
@@ -47,6 +48,20 @@ const ProjectContent = () => {
             className="bg-yellow-500"
           />
         </div>
+      </div>
+      <div className="mt-20 md:ml-[300px] xs:ml-[50px]  ">
+        <button
+          className="rounded-full group hover:border-2 transition-colors hover:bg-black  flex space-x-7 border-2 bg-transparent px-5 sm:px-10 py-2 border-black "
+          onClick={handleClick}
+        >
+          <IoLogoGithub
+            className="text-black group-hover:text-white"
+            size={30}
+          />
+          <span className="text-xl group-hover:text-white">
+            See All Projects
+          </span>
+        </button>
       </div>
     </div>
   );
